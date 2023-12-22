640f52068d09494b72ff52591739127d1af725d0
flaviusgavriluta <flavius.gavriluta@gmail.com>
2023-12-22 12:02:23 +0200

    update Step 3

--- scc/src/main/java/com/codecool/scc/ConverterApplication.java
+++ scc/src/main/java/com/codecool/scc/ConverterApplication.java
@@ -4,21 +4,15 @@ import com.codecool.scc.model.OutputFormat;
 
 public class ConverterApplication {
     public static void main(String[] args) {
-        OutputFormat outputFormat = OutputFormat.TABLE;
         String csvFilePath = null;
 
         switch (args.length) {
-            case 1 -> csvFilePath = args[0];
-            case 2 -> {
-                outputFormat = OutputFormat.valueOf(args[0].toUpperCase());
-                csvFilePath = args[1];
-            }
-            default -> {
-                System.out.println("Invalid arguments");
+            case 0 -> {
+                System.out.println("No input file defined");
                 System.exit(1);
             }
+            case 1 -> csvFilePath = args[0];
         }
-        System.out.println("Output Format: " + outputFormat);
         System.out.println("CSV File Path: " + csvFilePath);
     }
 }
