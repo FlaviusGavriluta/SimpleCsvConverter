bc97a5f713897cd755a45330cb3d347d3de04c35
flaviusgavriluta <flavius.gavriluta@gmail.com>
2023-12-24 13:51:31 +0200

    update Step 3: No input file action"

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -4,15 +4,22 @@ import com.codecool.scc.model.OutputFormat;
 
 public class ConverterApplication {
     public static void main(String[] args) {
+        OutputFormat outputFormat = OutputFormat.TABLE;
         String csvFilePath = null;
-
         switch (args.length) {
-            case 0 -> {
+            case 1:
+                csvFilePath = args[0];
+                break;
+            case 2:
+                outputFormat = OutputFormat.valueOf(args[0].toUpperCase());
+                csvFilePath = args[1];
+                break;
+            default:
                 System.out.println("No input file defined");
                 System.exit(1);
-            }
-            case 1 -> csvFilePath = args[0];
         }
+
+        System.out.println("Output Format: " + outputFormat);
         System.out.println("CSV File Path: " + csvFilePath);
     }
 }
