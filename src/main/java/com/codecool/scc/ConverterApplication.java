5ef71b4c9f67778851b8193bfc3845cd978dcbe7
flaviusgavriluta <flavius.gavriluta@gmail.com>
2024-02-05 20:47:24 +0200

    task 13 finished

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -2,54 +2,26 @@ package com.codecool.scc;
 
 import com.codecool.scc.model.OutputFormat;
 import com.codecool.scc.output.OutputFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.List;
 
+@Component
 public class ConverterApplication {
-    private OutputFormat outputFormat;
-    private String csvFilePath;
-    private OutputFormatterFactory formatterFactory;
+    private final OutputFormatterFactory formatterFactory;
 
-    public ConverterApplication(OutputFormat outputFormat, String csvFilePath, OutputFormatterFactory formatterFactory) {
-        this.outputFormat = outputFormat;
-        this.csvFilePath = csvFilePath;
+    @Autowired
+    public ConverterApplication(OutputFormatterFactory formatterFactory) {
         this.formatterFactory = formatterFactory;
     }
 
-    public static void main(String[] args) {
-        if (args.length < 1 || args.length > 2) {
-            System.out.println("Invalid number of arguments. Usage: java -jar app.jar [format] file.csv");
-            System.exit(1);
-        }
-
-        OutputFormat outputFormat = OutputFormat.TABLE;
-        String csvFilePath = args[args.length - 1];
-
-        if (args.length == 2) {
-            outputFormat = getOutputFormat(args[0]);
-        }
-
-        OutputFormatterFactory outputFormatterFactory = new ConcreteOutputFormatterFactory();
-        ConverterApplication converterApplication = new ConverterApplication(outputFormat, csvFilePath, outputFormatterFactory);
-        converterApplication.run();
-    }
-
-    private static OutputFormat getOutputFormat(String format) {
-        try {
-            return OutputFormat.valueOf(format.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid output format. Supported formats: JSON, XML, and TABLE");
-            System.exit(1);
-            return null;
-        }
-    }
-
-    private void run() {
+    public void run(String csvFilePath, OutputFormat outputFormat) {
         System.out.println("Output Format: " + outputFormat);
         System.out.println("CSV File Path: " + csvFilePath);
-
-        String csvFilePath = "path/to/your/sample.csv";
 
         List<String[]> csvData = FileReaderClass.readData(new File(csvFilePath));
         System.out.println("Data from CSV file:");
@@ -62,8 +34,33 @@ public class ConverterApplication {
         } else {
             System.out.println("Invalid output format. Using default format: TABLE");
         }
+    }
 
-        SimpleCsvConverter csvConverter = new SimpleCsvConverter(formatterFactory);
-        csvConverter.convert(new File(csvFilePath), outputFormat);
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ConverterApplication converterApplication = context.getBean(ConverterApplication.class);
+
+        if (args.length < 1 || args.length > 2) {
+            System.out.println("Invalid number of arguments. Usage: java -jar app.jar [format] file.csv");
+            System.exit(1);
+        }
+
+        OutputFormat outputFormat = OutputFormat.TABLE;
+        if (args.length == 2) {
+            outputFormat = getOutputFormat(args[0]);
+        }
+        String csvFilePath = args[args.length - 1];
+
+        converterApplication.run(csvFilePath, outputFormat);
+    }
+
+    private static OutputFormat getOutputFormat(String format) {
+        try {
+            return OutputFormat.valueOf(format.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid output format. Supported formats: JSON, XML, and TABLE");
+            System.exit(1);
+            return null;
+        }
     }
 }
